5568d62f091313fdd800634c3c8dcd7d95496574
ahorts <godswayahorts@gmail.com>
2026-04-20 23:08:31 +0000

    feat(cryptoRoutes): add route to get crypto by symbol

--- src/routes/cryptoRoutes.js
+++ src/routes/cryptoRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get('/', cryptoController.getAllCrypto);
 router.get('/gainers', cryptoController.getGainers);
 router.get('/new', cryptoController.getNewListings);
+router.get('/:symbol', cryptoController.getCryptoBySymbol);
 router.post('/', cryptoController.addCrypto);
 
 module.exports = router;
